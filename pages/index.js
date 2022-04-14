090bf17c318c6ff7be9b2c1be34e786fb30bea6b
yoidea <yoidea.cc@gmail.com>
2022-04-15 06:08:59 +0900

    Implement edit page

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,118 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import { useState, useEffect } from 'react'
 
 export default function Home() {
+  const [tweetId, setTweetId] = useState('');
+  const [sid, setSid] = useState('');
+  const [question, setQuestion] = useState('');
+  const [choices1, setChoices1] = useState('');
+  const [choices2, setChoices2] = useState('');
+  const [choices3, setChoices3] = useState('');
+  const [choices4, setChoices4] = useState('');
+  const [answer, setAnswer] = useState(1);
+
+  useEffect(() => {
+    const sid = tweetId.split('/')[5];
+    setSid(sid);
+    const src = `https://platform.twitter.com/embed/Tweet.html?id=${sid}`;
+    if (document.querySelector("#twitter-widget-0")) {
+      document.querySelector("#twitter-widget-0").src = src;
+    }
+  }, [tweetId]);
+
   return (
     <div className="container">
       <Head>
         <title>tweet2test</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css" />
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
       </Head>
 
       <main>
         <h1 className="title">
-          tweet2test
+          問題作成
         </h1>
-        <p className="description">
-          ツイート文を題材に選択問題を作れるサービス
-        </p>
+        ツイートURL: 
+        <input value={tweetId} onChange={(e) => {
+          setTweetId(e.target.value);
+        }} type="text" />
+        <blockquote className="twitter-tweet">
+          <a id="emb" href="https://twitter.com/yoidea/status/1509714864737988619" />
+        </blockquote>
+        <nav class="panel">
+          <p class="panel-heading">
+            <strong>問題文: </strong>
+            <input value={question} onChange={(e) => {
+              setQuestion(e.target.value);
+            }} type="text" />
+          </p>
+          <div class="panel-block">
+            <strong>(ア)&nbsp;&nbsp;</strong>
+            <input value={choices1} onChange={(e) => {
+              setChoices1(e.target.value);
+            }} type="text" />
+            <div class="control has-text-right">
+              <label class="radio">
+                <input value="1" onChange={() => {
+                  setAnswer(1);
+                }} type="radio" name="answer" />
+                正解
+              </label>
+            </div>
+          </div>
+          <div class="panel-block">
+            <strong>(イ)&nbsp;&nbsp;</strong>
+            <input value={choices2} onChange={(e) => {
+              setChoices2(e.target.value);
+            }} type="text" />
+            <div class="control has-text-right">
+              <label class="radio">
+                <input value="2" onChange={() => {
+                  setAnswer(2);
+                }} type="radio" name="answer" />
+                正解
+              </label>
+            </div>
+          </div>
+          <div class="panel-block">
+            <strong>(ウ)&nbsp;&nbsp;</strong>
+            <input value={choices3} onChange={(e) => {
+              setChoices3(e.target.value);
+            }} type="text" />
+            <div class="control has-text-right">
+              <label class="radio">
+                <input value="3" onChange={() => {
+                  setAnswer(3);
+                }} type="radio" name="answer" />
+                正解
+              </label>
+            </div>
+          </div>
+          <div class="panel-block">
+            <strong>(エ)&nbsp;&nbsp;</strong>
+            <input value={choices4} onChange={(e) => {
+              setChoices4(e.target.value);
+            }} type="text" />
+            <div class="control has-text-right">
+              <label class="radio">
+                <input value="4" onChange={() => {
+                  setAnswer(4);
+                }} type="radio" name="answer" />
+                正解
+              </label>
+            </div>
+          </div>
+        </nav>
+        <div className="has-text-centered">
+          <Link href={`/${sid}?q=${question}&c1=${choices1}&c2=${choices2}&c3=${choices3}&c4=${choices4}&a=${answer}`}>
+            <a className="button is-success">
+              完成
+            </a>
+          </Link>
+        </div>
       </main>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-        .title,
-        .description {
-          text-align: center;
-        }
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
